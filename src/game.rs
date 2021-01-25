ff2216d7a7acabf4b1e07dfcb6227acd75322fe3
Scott Fones <scott.fones@gmail.com>
2021-01-25 16:59:30 -0500

    move fns into struct impl

--- src/game.rs
+++ src/game.rs
@@ -1,17 +1,81 @@
 /// Contains functions defining the game and valid moves.
-
 use peroxide::prelude::*;
+
+/// Explicit player value
+#[derive(Debug)]
+pub enum Player {
+    Max,
+    Min,
+}
+
+impl Player {
+    pub fn next_player(&self) -> Player {
+        match self {
+            Player::Max => Player::Min,
+            Player::Min => Player::Max,
+        }
+    }
+}
+
+/// Store current player and board state
+#[derive(Debug)]
+pub struct GameState {
+    pub player: Player,
+    pub board: Matrix,
+}
+
+impl GameState {
+    pub fn advance(&self, src: Location) -> GameState {
+        let new_n;
+
+        match self.player {
+            Player::Max => new_n = src.n + 1,
+            Player::Min => new_n = src.n - 1,
+        }
+
+        let dst = Location{m: src.m, n: new_n};
+        self.update(dst, src)
+    }
+
+    pub fn update(&self, dst: Location, src: Location) -> GameState {
+        let p = self.player.next_player();
+        let mut b = self.board.clone();
+
+        b[(dst.m, dst.n)] = self.board[(src.m, src.n)];
+        b[(src.m, src.n)] = 0.0;
+
+        GameState{player: p, board: b}
+    }
+}
 
 /// m by n index values within the game matrix
 #[derive(Debug)]
-struct Location{
-    m: u8,
-    n: u8,
+pub struct Location {
+    pub m: usize,
+    pub n: usize,
 }
 
-pub fn new_game() {
-    let board = matrix(c!(1,1,1,0,0,0,-1,-1,-1), 3, 3, Row);
-    println!("{}", board);
+impl Location {
+    pub fn advance(&self, s: &GameState) -> Location {
+        let player = &s.player;
+
+        match player {
+            Player::Max => Location{m: self.m+1, n: self.n},
+            Player::Min => Location{m: self.m-1, n: self.n}
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        (0..3).contains(&self.m) && (0..3).contains(&self.n)
+    }
 }
 
-
+/// Initialize new game state
+pub fn new_game() -> GameState {
+    let p = Player::Max;
+    let b = matrix(c!(1, 1, 1, 0, 0, 0, -1, -1, -1), 3, 3, Row);
+    GameState {
+        player: p,
+        board: b,
+    }
+}
