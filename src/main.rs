d0fea4a53cf74c9b771a57d7037b79ed39dc3a9d
Scott Fones <scott.fones@gmail.com>
2021-01-29 22:31:58 -0500

    display GameState instead of board

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ pub mod game;
 fn main() {
     let g0 = game::new_game();
     let l = game::Location { m: 0, n: 0 };
-    println!("{}", g0.board);
+    println!("{}", g0);
 
     if l.check_advance(&g0) {
         let g1 = g0.advance(l);
-        println!("{}", g1.board);
+        println!("{}", g1);
     }
 }
